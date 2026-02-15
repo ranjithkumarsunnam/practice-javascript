29ebf79acce61bd4932a02914e259a65ab0e274e
ranjithkumarsunnam <sunnamranjith7@gmail>
2026-02-15 19:51:50 +0530

    java script methods practice

---
+++ arraymethods.js
@@ -0,0 +1,507 @@
+// method push add element at end of array 1
+let fruits = ["apple", "banana", "orange", "grapess"];
+fruits.push("mango");
+console.log(fruits);
+
+//method push for example array 2
+let numbers = [1, 2, 3, 4];
+numbers.push(5);
+console.log(numbers);
+
+//method push for example array 3
+let names = ["ramu", "raki", "rakko", "ralex"];
+names.push("ranjith");
+console.log(names);
+
+//method push for example array 4
+
+let mobiles = ["sony", "iqoo", "vivo"];
+mobiles.push("mi");
+console.log(mobiles);
+
+//method push example 5 for array 5
+
+let items = ["pen", "pencil", "eraser"];
+items.push("sharpner");
+console.log(items);
+
+//method push method for array example 6
+let colors = ["red", "orange", "greem", "blue"];
+colors.push("black");
+console.log(colors);
+
+//method push example 7 for array 7
+
+let cities = ["heyderabad", "nagole", "ammerpet"];
+cities.push("lb nagar");
+console.log(cities);
+
+//example 8
+let animals = ["lion", "tiger", "fox"];
+animals.push("lion");
+console.log(animals);
+
+//ex 9
+let flowers = ["rose", "lilly"];
+flowers.push("sunflowerr");
+console.log(flowers);
+
+//ex 10
+let subjects = ["maths", "science"];
+subjects.push("english");
+console.log(subjects);
+
+//method pop
+let fruit = ["apple", "bannana", "orange"];
+fruit.pop();
+console.log(fruit);
+
+//pop ex 2
+
+let numberss = [1, 2, 3];
+numberss.pop();
+console.log(numberss);
+
+//pop ex 3
+let namess = ["raa", "raki", "rammkoo"];
+namess.pop();
+console.log(namess);
+
+//pop ex 4
+let mobiless = ["sony", "iqoo", "vivo"];
+mobiless.pop();
+console.log(mobiless);
+
+//pop 5
+let itemss = ["pen", "pencil", "earaser"];
+itemss.pop();
+console.log(itemss);
+
+//pop ex 6
+let colorss = ["red", "blue", "black"];
+colorss.pop();
+console.log(colorss);
+
+//pop ex 7
+let citiess = ["hyderabad", "nagole", "ammerpet"];
+citiess.pop();
+console.log(citiess);
+
+//pop ex 8
+let animalss = ["lion", "tiger", "fox"];
+animalss.pop();
+console.log(animalss);
+
+//pop ex 9
+let flowerss = ["rose", "lilly", "sunflower"];
+flowerss.pop();
+console.log(flowerss);
+
+//pop ex 10
+let subjectss = ["maths", "science", "social"];
+subjectss.pop();
+console.log(subjectss);
+
+//method shift is ued to delete the first element in array
+let fruits1 = ["apple", "banana", "orange"];
+fruits1.shift();
+console.log(fruits1);
+
+//shift ex2
+let numbers1 = [1, 2, 3];
+numbers1.shift();
+console.log(numbers1);
+//shift ex 3
+let names1 = ["ramu", "raki", "rakko"];
+names1.shift();
+console.log(names1);
+
+//shift ex 4
+let mobiles1 = ["vivo", "redmi", "oppo"];
+mobiles1.shift();
+console.log(mobiles1);
+
+//shift ex 5
+let items1 = ["pen", "pencil", "rubber"];
+items1.shift();
+console.log(items1);
+
+//shift ex 6
+let colors1 = ["red", "blue", "green"];
+colors1.shift();
+console.log(colors1);
+
+//shift ex 7
+let cities1 = ["hereabad", "nagolr", "lb nagar"];
+cities1.shift();
+console.log(cities1);
+
+//shift ex 8
+let animales1 = ["lion", "tiger", "for"];
+animales1.shift();
+console.log(animales1);
+//shift ex 9
+let flowers1 = ["rose", "lilly", "sunflower"];
+flowers1.shift();
+console.log(flowers1);
+
+//shift ex 10
+let subjects1 = ["maths", "social", "science"];
+subjects1.shift();
+console.log(subjects1);
+
+//method unshift is used to add element at the beginning of array 1
+let fruits2 = ["apple", "kiwi", "orange"];
+fruits2.unshift("banana");
+console.log(fruits2);
+
+//unshift ex 2
+let numbers2 = [1, 2, 3, 4, 5];
+numbers2.unshift(2);
+console.log(numbers2);
+
+//unshift ex 3
+let names2 = ["rami", "sumi", "rako"];
+names2.unshift("ranjit");
+console.log(names2);
+
+//unshift ex 4
+let apps = ["face", "insta", "teitter"];
+apps.unshift("whatapp");
+console.log(apps);
+
+//unshift ex 5
+let user = ["name", "age"];
+user.unshift("id");
+console.log(user);
+//unshift ex 6
+let logo = ["nikr", "pumr", "adidas"];
+logo.unshift("reebook");
+console.log(logo);
+
+//unshift ex 7
+let cities2 = ["hyd", "knr", "gdk"];
+cities2.unshift("khammam");
+console.log(cities2);
+//unshift ex 8
+let books = ["java", "py", "c"];
+books.unshift("js");
+console.log(books);
+
+//unshift ex 9
+let flow = ["red", "blur", "green"];
+flow.unshift("yellow");
+console.log(flow);
+
+//unshift 10
+
+let hot = ["tea", "coffe"];
+hot.unshift("milk");
+console.log(hot);
+
+//method length is used to determine the number of elements in an array
+let students = ["ramu", "raki", "rakko", "ranjith"];
+console.log(students.length);
+
+//ex 2
+let marks = [10, 20, 30, 40, 50];
+console.log(marks.length);
+
+//ex 3
+let namesss = ["ramu", "raki", "rakko"];
+console.log(names.length);
+
+//ex 4
+let mobile2 = ["vio", "mi", "oppo"];
+console.log(mobile2.length);
+
+//ex 5
+let items2 = ["pem", "pencil", "rubrer"];
+console.log(items2.length);
+
+//ex 6
+let movies = ["kfg", "bahubalio", "remo", "raksiko"];
+console.log(movies.length);
+
+let color2 = ["red", "ble", "black", "green"];
+console.log(color2.length);
+
+//ex 8
+let city = ["hf", "knr", "gdj"];
+console.log(city.length);
+//ex9
+
+let books1 = ["java", "c", "py"];
+console.log(books1.length);
+
+//ex 10
+let cards = ["heart", "blue", "red", "black", "bubble"];
+console.log(cards.length);
+
+//includes
+
+//ex1 for includers
+
+let fruites = ["apple", "banana", "manngo"];
+console.log(fruites.includes("apple"));
+
+//ex 2for includes
+
+let names3 = ["1", "2", "3", "4", "5"];
+console.log(names3.includes("3"));
+
+//ex 3
+
+let movies1 = ["kgf", "ala", "maska"];
+console.log(movies1.includes("aha"));
+
+// ex 4
+
+let folwers = ["rose", "lilly", "sunflower"];
+console.log(folwers.includes("jasmine"));
+
+//ex 5
+let mobiles2 = ["vivo", "oppo", "mi"];
+console.log(mobiles2.includes("iqoo"));
+
+//let ex 6
+
+let animals2 = ["lion", "monkey", "donkey"];
+console.log(animals2.includes("monkey"));
+
+//ex 7
+
+let phones = ["mi", "vivo", "opp", "realme"];
+console.log(phones.includes("realme"));
+
+//ex 8
+
+let marks2 = [20, 30, 40, 50];
+console.log(marks2.includes(20));
+
+//ex 9
+let students2 = ["ramu", "loki", "kaki", "jacy"];
+console.log(students2.includes("kaki"));
+
+//ex 10
+
+let sex = ["gender", "male", "female", "others"];
+console.log(sex.includes("gender"));
+
+//method indexof is used to find the index of element in array
+
+//ex 1for indexOf
+
+let fruits3 = ["apple", "banana", "mango"];
+console.log(fruits3.indexOf("banana"));
+
+//ex 2
+
+let usersname = ["saikumar", "ranjithkumar", "adhrash"];
+console.log(usersname.indexOf("ranjithkumar"));
+console.log(usersname.indexOf("adhrash"));
+console.log(usersname.indexOf("saikumar"));
+
+//ex 3
+
+let food = ["biriyani", "chicken", "mutton"];
+console.log(food.indexOf("chicken"));
+
+//ex 4
+let marks3 = [10, 20, 30, 40, 50];
+console.log(marks3.indexOf(50));
+
+//ex 5
+let usernames = ["sai", "kumar", "ranjith", "kumar"];
+console.log(usernames.indexOf("kumar"));
+
+//ex 6
+let mails = ["mails", "letters", "post"];
+console.log(mails.indexOf("post"));
+
+//ex 7
+let online = ["amazon", "fliptkart", "myntra"];
+console.log(online.indexOf("myntra"));
+
+//ex 8
+let colors3 = ["red", "blue", "green"];
+console.log(colors3.indexOf("blue"));
+
+//ex 9
+let cities3 = ["hyd", "knr", "gdk"];
+console.log(cities3.indexOf("knr"));
+
+//ex 10
+let books3 = ["java", "com", "py"];
+console.log(books3.indexOf("java"));
+
+//slice method used in array to extract a portion of an array and return a new array
+
+//ex 1 for slice
+
+let nums = [1, 2, 3, 4, 5];
+console.log(nums.slice(1, 3));
+
+//ex 2
+
+let names4 = ["ramu", "raki", "rom", "ranjith", "sai", "adhi"];
+console.log(names4.slice(0, 5));
+
+//ex 3
+let password = ["123456", "12365", "4564fb", "6384dsingh"];
+console.log(password.slice(0, 2));
+
+//ex 4
+
+let directions = ["north", "south", "east", "west"];
+console.log(directions.slice(0, 3));
+
+//ex 5
+let colors4 = ["red", "blue", "green", "yellow"];
+console.log(colors4.slice(1, 3));
+//ex 6
+let form = ["form", "input", "button"];
+console.log(form.slice(0, 2));
+
+//ex 7
+let conditions = ["hot", "rainy", "cold", "hot"];
+console.log(conditions.slice(1, 3));
+
+//ex 8
+let fronts = ["front1", "front2", "front3"];
+console.log(fronts.slice(0, 2));
+
+//ex 9
+let bags = ["bag1", "bag2", "bag3", "bag4", "bag5", "bag6"];
+console.log(bags.slice(2, 7));
+
+//ex 10
+
+let shoes = ["shoe1", "shoe2", "shoe3", "shoe4"];
+console.log(shoes.slice(1, 3));
+
+//method splice is used to add or remove elements from an array
+
+//ex 1 splice for add element in array
+let fruits4 = ["apple", "banana", "orange"];
+fruits4.splice(1, 0, "mango");
+console.log(fruits4);
+
+//ex 2 splice for add element in array
+let names5 = ["ramu", "raki", "rakko"];
+names5.splice(2, 0, "ranjith");
+console.log(names5);
+
+//ex 3 splice for add element in array
+let mobiles3 = ["vivo", "redmi", "oppo"];
+mobiles3.splice(1, 0, "mi");
+console.log(mobiles3);
+
+//ex 4 splice for add element in array
+let items3 = ["pen", "pencil", "rubber"];
+items3.splice(2, 0, "sharpner");
+console.log(items3);
+
+//ex 5
+let colors5 = ["red", "blue", "green"];
+colors5.splice(2, 1, "black");
+console.log(colors5);
+
+// let splice remove element from array
+//ex 1
+let fruits5 = ["apple", "banana", "orange"];
+fruits5.splice(1, 1);
+console.log(fruits5);
+
+//ex 2
+let names6 = ["ramu", "raki", "rakko"];
+names6.splice(2, 2);
+console.log(names6);
+
+//ex 3
+let furniture = ["table", "chair", "sofa"];
+furniture.splice(1, 1);
+console.log(furniture);
+
+//ex 4
+let books2=["book1","book2","book3"];
+books2.splice(0,1);
+console.log(books2);
+
+//ex 5
+let colors6=["red","blue","green"];
+colors6.splice(1,0);
+console.log(colors6);
+
+
+//let replace element in array
+//ex 1
+let fruits6 = ["apple", "banana", "orange"];
+fruits6.splice(1, 1, "mango");
+console.log(fruits6);
+
+//ex 2
+let names7 = ["ramu", "raki", "rakko"];
+names7.splice(2, 1, "ranjith");
+console.log(names7);
+
+//ex 3
+let mobiles4 = ["vivo", "redmi", "oppo"];
+mobiles4.splice(1, 1, "mi");
+console.log(mobiles4);
+
+//ex 4
+let a = ["pen", "pencil", "rubber"];
+a.splice(2, 1, "sharpner");
+console.log(a);
+
+//ex 5
+let b = ["red", "blue", "green"];
+b.splice(2, 1, "black");
+console.log(b);
+
+//for each method is used to execute a provided function once for each array element
+
+//ex 1 for each
+let fruits7 = ["apple", "banana", "orange"];
+fruits7.forEach(function(fruit){
+    console.log(fruit);
+});
+
+//ex 2 for each
+let numbers3=[1,2,3,4,5];
+numbers3.forEach((n)=>{
+    console.log(n);
+});
+
+//ex 3 
+let num =[1,5,3,4,7,8,9,3];
+
+num.forEach(function(number){
+    console.log(number);
+
+});
+
+//ex 4
+
+let num1 =[5,6,7,8,9];
+
+num1.forEach(function(n){
+    console.log(n);
+
+});
+
+//let ex 5
+
+let names8 =["ram","raki","adhi","vishwa","sai"];
+
+names8.forEach((n)=>{
+    console.log(n);
+});
+
+//ex 6
+
+let emails =["admin@gmail","ramu1@email","raki33@gmail.com"];
+emails.forEach((emails)=>{
+console.log(emails)
+});
---
+++ methods.js
@@ -0,0 +1,225 @@
+// creating an array for 5 fruites
+const fruites = ["apple", "banana", "grapes", "papaya", "kiwi"];
+console.log(fruites[0]); // print index '0'
+console.log(fruites.push("mango")); // push new item into array 'mango'
+console.log(fruites.pop()); //removing the last item in array
+console.log(fruites.includes("apple")); //apple includes(present) or not in array
+console.log(fruites.shift());
+console.log(fruites.unshift("new fruit"));
+console.log(fruites.indexOf("banana"));
+console.log(fruites.length);
+
+//creating an array using student names
+const StudentsNames = ["ramu", "raki", "raja", "ranjith", "vishwa"];
+console.log(StudentsNames[0]);
+console.log(StudentsNames.push("adhi"));
+console.log(StudentsNames.pop());
+console.log(StudentsNames.includes("ranjith"));
+console.log(StudentsNames.indexOf("raja"));
+console.log(StudentsNames.shift());
+console.log(StudentsNames.unshift("roko"));
+console.log(StudentsNames.indexOf("ranjith"));
+console.log(StudentsNames.length);
+
+//creating an array for user id
+const userid = ["123", "456", "789", "147", "258", "369"];
+
+const result = userid.indexOf("123");
+console.log(result);
+
+const result1 = userid.pop();
+console.log(result1);
+
+const id = userid.shift();
+console.log(id);
+
+const id1 = userid.unshift("789");
+console.log(id1);
+
+const lengths = userid.length;
+console.log(lengths);
+
+console.log(userid.includes("456"));
+console.log(userid[5]);
+console.log(userid.push("258"));
+
+console.log(userid.indexOf("456"));
+
+//subjects of students
+
+const subjects = ["math", "science", "social", "telugu", "hindi"];
+
+const a = subjects.pop();
+console.log(a);
+
+const b = subjects.push("marathi");
+console.log(b);
+
+const c = subjects.shift();
+console.log(c);
+
+const d = subjects.length;
+console.log(d);
+
+const e = subjects.unshift("urdu");
+console.log(e);
+
+const aa = subjects.indexOf("social");
+console.log(aa);
+
+const bb = subjects.includes("telugu");
+console.log(bb);
+
+//diff console log
+console.log(subjects.pop());
+console.log(subjects.push("mango"));
+console.log(subjects.includes("telugu"));
+console.log(subjects.indexOf("hindi"));
+console.log(subjects.length);
+console.log(subjects.shift());
+console.log(subjects.unshift("room"));
+console.log(subjects.join(","));
+
+// examples method in Strings
+
+const name = "ranjith";
+const names = name.toUpperCase();
+console.log(names);
+
+const lowercase = name.toLowerCase();
+console.log(lowercase);
+
+const Length = name.length;
+console.log(Length);
+
+const Include = name.includes("java");
+console.log(Include);
+
+const Slice = name.slice(0, 6);
+console.log(Slice);
+
+const Replace = name.replace("ranjith", "hello");
+console.log(Replace);
+
+const Trim = name.trim("ranjith");
+console.log(Trim);
+
+const Split = name.split("").reverse().join();
+console.log(Split);
+
+// example 2 fo strings
+const room = "abcde";
+
+console.log(room.toUpperCase());
+console.log(room.toLowerCase());
+console.log(room.slice(0, 5));
+console.log(room.length);
+console.log(room.split(""));
+console.log(room.trim());
+console.log(room.split("").reverse().join(""));
+console.log(room.includes("bcd"));
+
+// example 3
+
+const lett = "adhrash";
+console.log(lett.toUpperCase());
+console.log(lett.toLowerCase());
+console.log(lett.length);
+console.log(lett.includes("dhrash"));
+console.log(lett.slice(0, 5));
+console.log(lett.split(""));
+console.log(lett.trim());
+console.log(lett.split("").reverse().join());
+
+//example 4
+
+const nam = "vishwanath";
+console.log(nam.toUpperCase());
+console.log(nam.toLowerCase());
+console.log(nam.length);
+console.log(nam.slice(0, 4));
+console.log(nam.split(""));
+console.log(nam.split("").reverse().join());
+console.log(nam.trim());
+console.log(nam.includes("nath"));
+console.log(nam.split("").reverse());
+
+//Object in javaScript
+
+const students1 = {
+  name: "ranjith",
+  age: "20",
+  course: "cse",
+};
+console.log(students1.name);
+console.log(students1.age);
+console.log(students1.course);
+console.log(students1);
+
+//updated age
+students1.age = "22";
+console.log(students1);
+
+//update course
+students1.course = "react";
+console.log(students1);
+//delete the key and vales of the student
+delete students1.age;
+console.log(students1);
+
+//real example
+
+let user = {
+  id: "1",
+  name1: "mahesh",
+  role: "admin",
+};
+console.log(user.name1);
+console.log(user.id);
+console.log(user.role);
+console.log(user);
+//update name
+user.name1 = "ranjith";
+console.log(user);
+//update role
+user.role = "manager";
+console.log(user);
+
+//example
+const car = {
+  brand: "tata",
+  model: "2021",
+  price: 50000,
+};
+
+console.log(car);
+console.log(car.model);
+console.log(car.price);
+
+//udating car brand
+car.brand = "i20";
+console.log(car);
+
+//update price
+car.price = 100000;
+console.log(car);
+
+//update os model
+car.model = "2020";
+console.log(car);
+
+//add color property
+car.color = "blue";
+console.log(car);
+
+const students = [
+  { names: "ranjith", age: "20", role: "student" },
+  { names: "raju", age: "22", role: "farmer" },
+  { names: "adhrash", age: "30", role: "student" },
+  { names: "raki", age: "22", role: "student" },
+];
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i].names);
+}
+
