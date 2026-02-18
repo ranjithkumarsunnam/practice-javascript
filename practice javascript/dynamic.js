206c2d4258105b9ffe65448c12d107ee29b0b62f
ranjithkumarsunnam <sunnamranjith7@gmail>
2026-02-18 07:53:38 +0530

    new content is added

--- practice javascript/dynamic.js
+++ practice javascript/dynamic.js
@@ -1,10 +1,14 @@
-const updateContentButton = document.getElementById("updateContentBtn");
+/*const updateContentButton = document.getElementById("updateContentBtn");
 const dynamicContentDiv = document.getElementById("dynamicContent");
 
 const Button = document.getElementById("button1");
-const Update =document.getElementById("update");
+const Update =document.getElementById("update");*/
 
-updateContentButton.addEventListener("click", () => {
+const ElementsContainer = document.getElementById("elementsContainer"); //new
+const CreateElementBtn = document.getElementById("createElementBtn"); //new
+const DeleteFirstElementBtn = document.getElementById("deleteFirstElementBtn");
+
+/*updateContentButton.addEventListener("click", () => {
   dynamicContentDiv.innerHTML = "<p> ranjith</p>";
 });
 
@@ -12,3 +16,22 @@ Button.addEventListener("click",() =>{
 Update.innerHTML = "<p>welcome to my show</p>";
 
 });
+
+*/
+let elementCount = 0;
+CreateElementBtn.addEventListener('click', () => {
+  elementCount++;
+  const newpPragraph = document.createElement("p");
+
+  const newParagraph = document.createElement("p");
+  newParagraph.textContent = "Dynamically created element elementCount";
+  ElementsContainer.appendChild(newParagraph);
+});
+
+DeleteFirstElementBtn.addEventListener("click", () => {
+  if (ElementsContainer.children.length > 0) {
+    ElementsContainer.removeChild(ElementsContainer.children[0]);
+  }
+});
+
+
