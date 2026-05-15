1907ca102c1a4007616597325d3808571a5e83ae
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-15 20:23:46 +0530

    typecasting in javascript practice session

---
+++ typecasting.js
@@ -0,0 +1,34 @@
+//Implicit TypeCasting -->automatically js can convert
+let a = "10";
+let b =5;
+let result =a+b;
+console.log(result);
+console.log(typeof result)
+
+//typeof is keyword which show datatype
+
+
+
+
+
+
+//Explicit Typecasting -->manually developer can convert 
+/* let a ="10";
+let b =10;
+let c=a+b;
+console.log( typeof c);
+console.log(parseInt(c));
+console.log(typeof parseInt(c));
+
+
+
+let num = "100";
+let result =Number(num);
+console.log(result);
+console.log(typeof num);
+console.log(typeof result); */
+
+let num1 = "22.25";
+num2 =parseInt(num1);
+console.log(num2);
+
