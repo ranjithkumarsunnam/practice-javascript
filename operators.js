419a714c05b6f9f8504210b113e21b263d729c9a
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-17 12:52:14 +0530

    Operators in javascript practice

---
+++ operators.js
@@ -0,0 +1,50 @@
+//Operators in javaScript 
+
+//Arthematic Operators
+//comparison Operators
+//Logical Operators
+//Assignment Operators
+
+/* let x = "10";
+let y = 10;
+let add = x+y;
+let sub = x-y;
+let mul = x*y;
+let div = x/y;
+let mod = x%y;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+
+//Comparison Operator
+console.log(x==y);
+console.log(x===y);
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
+
+//Logical Operators
+//OR ----->Addition operation
+console.log(2===2 || 2==2);// t || t =t
+console.log(2==5 || 2==2); //f || t = t
+console.log(2>=2 || 2>5);//t || f =t
+console.log(2===3 || 2>5);//f ||f =f
+           //false || true --> true */
+           // F || T--> T
+           // T || T--> T
+           // F ||F --> F
+  //AND && ---->Multiplication Operation
+console.log(2===2 && 2==2);// t || t =t
+console.log(2==5 && 2==2); //f || t = f
+console.log(2>=2 && 2>5);//t || f =f
+console.log(2===3 && 2>5);//f ||f =f
+
+//Assignment Operators
+
+let z =100; // = ---> is assignment Operator
+console.log(z);
