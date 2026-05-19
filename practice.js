27898854cd8989a723414b62d9ec81c7c6031e68
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-19 22:40:14 +0530

    practice revision today

---
+++ practice.js
@@ -0,0 +1,69 @@
+//Multiplication of number and string numbers
+let a="10";
+let b="10";
+
+let result =a*parseInt(b);
+// console.log(parseInt(a)*parseInt(b));
+//let c =parseInt(b);
+
+console.log(a*b);
+console.log(typeof result);
+
+let result2 =Number(a)*Number(b);
+console.log(result2)
+console.log(typeof result2)
+//Boolean 
+let b1 =Boolean(true);
+let b2 =Boolean(false);
+console.log(Number(b1));
+console.log(Number(b2));
+
+let b3 =Boolean(0);
+let b4 =Boolean(1)
+console.log(b3)
+console.log(b4)
+
+//Operators
+
+//Logical Operators
+//AND(multiplication)
+
+let i=10;
+let j=10;
+if(i==10 && j==11)
+{
+    console.log("equal")
+}
+else{
+    console.log("not equal");
+}
+
+
+//Locial Operators
+//OR
+
+if(i==10 || j==30){
+    console.log("10 is equal");
+}
+else{
+    console.log("10 is not equal");
+}
+
+console.log(i==2 || j===11)
+
+console.log(i<20);
+
+for(let i=0; i<=10; i++){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
