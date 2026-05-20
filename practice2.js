41658c25e56d282c741f65ec25153ea58817b2aa
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-05-20 22:52:51 +0530

    practice arrays,arraymethods,null,undefined,functions

---
+++ practice2.js
@@ -0,0 +1,40 @@
+//Arrays in javascript
+// let Arays =20;
+
+let Array =[1,2,3,4,5,6,7,8,9];
+
+//console.log(Array[0]);
+
+//Array Methods
+Array.push(10);
+//console.log(Array);
+
+
+//Shift Method in js
+Array.shift();
+console.log(Array)
+Array.shift();
+console.log(Array)
+
+
+//Unshift
+
+let ranjith="Ranjith";
+Array.unshift(ranjith);
+console.log(Array)
+
+//pop
+/* Array.pop()
+console.log(Array);
+//reverse
+Array.reverse();
+console.log(Array) */
+
+
+console.log( Array.length);
+
+// for(let i=0; i<8; i++){
+//     console.log(i);
+// }
+
+
