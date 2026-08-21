d6b818efdf1ac768e85d759b222289e659e8ec78
ranjithkumarsunnam <sunnamranjith7@gmail.com>
2026-08-21 22:16:44 +0530

    added the findinf the non repeating number in the array

--- practice javascript/function.js
+++ practice javascript/function.js
@@ -148,6 +148,27 @@ let result = add(5, 10);
 console.log(result);
 
 
+//finding the first non -repeating number
+let arr = [10, 20, 10, 30, 20, 40, 30];
+
+for (let i = 0; i < arr.length; i++) {
+
+    let count = 0;
+
+    for (let j = 0; j < arr.length; j++) {
+
+        if (arr[i] === arr[j]) {
+            count++;
+        }
+    }
+
+    if (count === 1) {
+        console.log("First non-repeated element:", arr[i]);
+        break;
+    }
+}
+
+
 
 
 
